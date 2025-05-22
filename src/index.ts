9ad528750ae493a5b11968376b34746c70863096
Davide Bizzi <davide.bizzi@unguess.io>
2025-05-22 17:52:26 +0200

    Ristruttura la configurazione del server MCP per migliorare la gestione delle capacità e il formato del codice

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,62 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { z } from "zod";
 import { scanViolations } from "./accessibilityChecker";
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 
-
-const server = new McpServer({
+const server = new McpServer(
+  {
     name: "Accessibility Information",
-    version: "1.0.0"
-});
+    version: "1.0.0",
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  }
+);
 
 server.tool(
-    "scan_accessibility",
-    {
-        url: z.string().url(),
-        violationsTag: z.array(z.string()),
-        viewport: z.object({
-            width: z.number().default(1920),
-            height: z.number().default(1080)
-        }).optional(),
-        shouldRunInHeadless: z.boolean().default(true)
-    },
-    async ({url, violationsTag, viewport, shouldRunInHeadless}) => {
-        const {report, base64Screenshot} = await scanViolations(url, violationsTag, viewport, shouldRunInHeadless);
+  "scan_accessibility",
+  {
+    url: z.string().url(),
+    violationsTag: z.array(z.string()),
+    viewport: z
+      .object({
+        width: z.number().default(1920),
+        height: z.number().default(1080),
+      })
+      .optional(),
+    shouldRunInHeadless: z.boolean().default(true),
+  },
+  async ({ url, violationsTag, viewport, shouldRunInHeadless }) => {
+    const { report, base64Screenshot } = await scanViolations(
+      url,
+      violationsTag,
+      viewport,
+      shouldRunInHeadless
+    );
 
-        return {
-            content: [
-                {
-                    type: "text",
-                    text: JSON.stringify({
-                        message: 'The image has been saved to your downloads.',
-                        ...report,
-                    }, null, 2)
-                },
-                {
-                    type: "image",
-                    data: base64Screenshot,
-                    mimeType: "image/png"
-                }
-            ],
-            isError: false
-        };
-    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(
+            {
+              message: "The image has been saved to your downloads.",
+              ...report,
+            },
+            null,
+            2
+          ),
+        },
+        {
+          type: "image",
+          data: base64Screenshot,
+          mimeType: "image/png",
+        },
+      ],
+      isError: false,
+    };
+  }
 );
 
 export default server;
